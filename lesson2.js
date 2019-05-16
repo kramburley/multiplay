f7cea32bf45560836a50ad1fd0be2ddf5cf4cc42
kramburley <akirasendo7_13@yahoo.com>
2019-05-16 14:25:40 -0700

    lessons1-4 tutorial on node

---
+++ lesson2.js
@@ -0,0 +1,9 @@
+//console.log(process.argv)
+
+sum = 0
+
+for (let index = 2; index < process.argv.length; index++) {
+    sum += Number(process.argv[index])
+}
+
+console.log(sum)
---
+++ lesson3.js
@@ -0,0 +1,17 @@
+var fs = require('fs')
+
+/*
+var buf = fs.readFileSync(process.argv[2])
+
+var str = buf.toString()
+
+var arr = str.split("\n")
+
+console.log(arr.length - 1)
+*/
+
+fs.readFile('lesson3.txt', 'utf8',function(err,data) {
+    if (err) throw err;
+    console.log(data.split("\n").length - 1);
+    
+})
