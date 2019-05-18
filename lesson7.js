379967d6fddfcfb26e69f550bbf3a48063a90ccb
kramburley <akirasendo7_13@yahoo.com>
2019-05-17 18:52:21 -0700

    completed lesson7

---
+++ lesson7.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.get(process.argv[2], function callback(response) {
+    response.on("data", function(data) {
+        console.log(data)
+    }).setEncoding('utf8')
+})
