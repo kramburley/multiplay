af198962eb60687e16c6793dd46bc3ceabc0cd1f
kramburley <akirasendo7_13@yahoo.com>
2019-05-18 08:59:52 -0700

    ran some trials on lesson 7/8

--- lesson7.js
+++ lesson7.js
@@ -1,7 +1,15 @@
 var http = require('http');
 
-http.get(process.argv[2], function callback(response) {
-    response.on("data", function(data) {
-        console.log(data)
-    }).setEncoding('utf8')
+// http.get(process.argv[2], function callback(response) {
+//     response.on("data", function(data) {
+//         console.log(data)
+//     }).setEncoding('utf8')
+// })
+
+
+
+http.get(process.argv[2], function(response) {
+    response.on('data', console.log);
+    response.on('error', console.error);
+    response.setEncoding('utf8');
 })
